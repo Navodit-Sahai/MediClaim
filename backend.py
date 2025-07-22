2390fd4adead8bf7aa930cf7fb18cd46240a0e82
Navodit-Sahai <sahainavodit781@gmail.com>
2025-07-23 01:32:34 +0530

    final commit

--- backend.py
+++ backend.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from pydantic_models import state
 from lang import build_graph
+import uvicorn
+import os
 app = FastAPI()
 
 @app.post('/summarize')
@@ -8,3 +10,7 @@ def summarizer(request: state):
     graph = build_graph()
     response = graph.invoke(request)
     return response["ref_ans"].content
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  
+    uvicorn.run("backend:app", host="0.0.0.0", port=port, reload=True)
