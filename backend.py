526a52bf81144d72d510197b1ba57a5f573e78e1
Navodit-Sahai <sahainavodit781@gmail.com>
2025-07-26 12:35:58 +0530

    13

--- backend.py
+++ backend.py
@@ -71,4 +71,4 @@ async def summarizer(
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run("backend:app", host="0.0.0.0", port=port, reload=False)
